768ee9a57f42b0ca465af1359f532fa0a56a413e
vish7466 <abhi.vish7466@gmail.com>
2023-12-18 17:55:22 +0530

    configures auto import sorting for eslint

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 
+import ReactDOM from 'react-dom/client'
+import { Toaster } from 'react-hot-toast'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+
+import App from './App.jsx'
+import store from './redux/store'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+        <Toaster />
+      </BrowserRouter>
+    </Provider>
 )
