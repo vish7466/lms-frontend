a8e47b6095346764168b9e3e62b22219ac55c22f
vish7466 <abhi.vish7466@gmail.com>
2023-12-19 18:15:00 +0530

    added about us page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 
 import Home from './pages/Home'
+import Aboutus from './pages/Aboutus'
 
 function App() {
 
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/about' element={<Aboutus />} />
     </Routes>
   )
 }
