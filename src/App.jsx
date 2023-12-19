c3743f99ff1ea1050f111f86e2e79b748498b770
vish7466 <abhi.vish7466@gmail.com>
2023-12-19 19:30:28 +0530

    added signup page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Aboutus from './pages/Aboutus'
 import Notfound from './pages/Notfound'
+import Signup from './pages/Signup'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/about' element={<Aboutus />} />
+      <Route path='/signup' element={<Signup />} />
       <Route path='*' element={<Notfound />} />
     </Routes>
   )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import authReducer from './slices/authSlice'
+import authReducer from './slices/authSlice';
 const store = configureStore({
     reducer: {
         auth: authReducer
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false}),
     devTools: true
 });
 
